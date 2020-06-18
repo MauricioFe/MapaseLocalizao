fc15f288fee1300e42781cae68d6c3ff3edc0102
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-18 15:47:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mapaselocalizao/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mapaselocalizao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
